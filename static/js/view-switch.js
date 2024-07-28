a675e8b177897d3334ade6e9b11098388cc9f796
daszka <daria195318@gmail.com>
2024-07-29 01:59:07 +0200

    implemented all views

--- static/js/view-switch.js
+++ static/js/view-switch.js
@@ -27,3 +27,63 @@ function hideList()
 {
     document.getElementById("view-list").style.display = "none";
 }
+
+function showMap()
+{
+    document.getElementById("view-map").style.display = "block";
+}
+
+function hideMap()
+{
+    document.getElementById("view-map").style.display = "none";
+}
+
+function showCalendar()
+{
+    document.getElementById("view-calendar").style.display = "block";
+}
+
+function hideCalendar()
+{
+    document.getElementById("view-calendar").style.display = "none";
+}
+
+function showGallery()
+{
+    document.getElementById("view-gallery").style.display = "block";
+}
+
+function hideGallery()
+{
+    document.getElementById("view-gallery").style.display = "none";
+}
+
+function showBudget()
+{
+    document.getElementById("view-budget").style.display = "block";
+}
+
+function hideBudget()
+{
+    document.getElementById("view-budget").style.display = "none";
+}
+
+function showMenu()
+{
+    document.getElementById("view-menu").style.display = "block";
+}
+
+function hideMenu()
+{
+    document.getElementById("view-menu").style.display = "none";
+}
+
+function showSettings()
+{
+    document.getElementById("view-settings").style.display = "block";
+}
+
+function hideSettings()
+{
+    document.getElementById("view-settings").style.display = "none";
+}
