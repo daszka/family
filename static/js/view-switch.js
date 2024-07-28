e79039ab267cda6fe60fffc6f8064bf2937a385a
daszka <daria195318@gmail.com>
2024-07-29 01:08:20 +0200

    initial commit

---
+++ static/js/view-switch.js
@@ -0,0 +1,19 @@
+function showBlog()
+{
+    document.getElementById("view-blog").style.display = "block";
+}
+
+function hideBlog()
+{
+    document.getElementById("view-blog").style.display = "none";
+}
+
+function showMessages()
+{
+    document.getElementById("view-messages").style.display = "block";
+}
+
+function hideMessages()
+{
+    document.getElementById("view-messages").style.display = "none";
+}
